f1c03bd02ac9a1f0026ac32c9c9811e5801197bf
maheshupplala <uppalamahesh804@gmail.com>
2024-05-04 13:40:13 +0530

    Tranfer methode in UpI class is modified

--- Mavenproj1/src/test/java/com/nt/payment/UpI.java
+++ Mavenproj1/src/test/java/com/nt/payment/UpI.java
@@ -4,6 +4,9 @@ public class UpI {
 	public void transfer(long number,long accno) {
 		  System.out.println("UpI.transfer()");
 		  System.out.println("payment is transefferd to  this account Number"+accno);
+		  System.out.println("jooiiuytrewasdfghj");
+		  int a=90;
+		  
 	}
 
 }
