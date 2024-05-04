22934930f14acf0cce2f4e1319dff9a2a40d59ef
maheshupplala <uppalamahesh804@gmail.com>
2024-05-04 07:17:11 +0530

    M1 methode is implement

--- Mavenproj1/src/test/java/com/nt/service/A.java
+++ Mavenproj1/src/test/java/com/nt/service/A.java
@@ -1,5 +1,9 @@
 package com.nt.service;
 
 public class A {
-
+ public void m1(int a,int b) {
+	 System.out.println("sum of two numbers is");
+	 int c= a+b;
+	 System.out.println("A.m1() sum is"+c );
+ }
 }
