ee5c3ed177c925b4d68df6265f0859e3fef8794e
maheshupplala <uppalamahesh804@gmail.com>
2024-05-04 13:20:40 +0530

    Upi-class is added

---
+++ Mavenproj1/src/test/java/com/nt/payment/UpI.java
@@ -0,0 +1,9 @@
+package com.nt.payment;
+
+public class UpI {
+	public void transfer(long number,long accno) {
+		  System.out.println("UpI.transfer()");
+		  System.out.println("payment is transefferd to  this account Number"+accno);
+	}
+
+}
